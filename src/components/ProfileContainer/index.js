da811b8be48a39ce4aff3ec0572f82f3680390ac
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-06-17 07:57:13 +0700

    change to static images

--- src/components/ProfileContainer/index.js
+++ src/components/ProfileContainer/index.js
@@ -1,17 +1,12 @@
 import React from "react";
+import ProfileImage from "assets/images/aldy-user.jpg";
 
 import "./index.css";
 
 export default function ProfileContainer() {
-  const urlProfileImage =
-    "https://storage.googleapis.com/profile_app_images/aldy-user.jpg";
   return (
     <div className="profile-container">
-      <img
-        className="custom-img-profile"
-        src={urlProfileImage}
-        alt="aldy-user"
-      />
+      <img className="custom-img-profile" src={ProfileImage} alt="aldy-user" />
       <h2 className="name">Aldycky Bagus Witjaksana</h2>
       <p className="job">Software Engineer</p>
     </div>
